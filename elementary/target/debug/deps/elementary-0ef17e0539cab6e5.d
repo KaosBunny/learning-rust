5a5c1e608769f2b625c55eba273e85325ac4660f
KaosBunny <Niamh.K.Ferns@gmail.com>
2022-02-20 20:10:52 +1300

    stuff is added?

---
+++ elementary/target/debug/deps/elementary-0ef17e0539cab6e5.d
@@ -0,0 +1,5 @@
+/home/niamh/Repos/personal/learning-rust/elementary/target/debug/deps/elementary-0ef17e0539cab6e5.rmeta: src/main.rs
+
+/home/niamh/Repos/personal/learning-rust/elementary/target/debug/deps/elementary-0ef17e0539cab6e5.d: src/main.rs
+
+src/main.rs:
---
+++ elementary/target/debug/deps/elementary-24af27cc77ee0a07.d
@@ -0,0 +1,5 @@
+/home/niamh/Repos/personal/learning-rust/elementary/target/debug/deps/elementary-24af27cc77ee0a07.rmeta: src/main.rs
+
+/home/niamh/Repos/personal/learning-rust/elementary/target/debug/deps/elementary-24af27cc77ee0a07.d: src/main.rs
+
+src/main.rs:
