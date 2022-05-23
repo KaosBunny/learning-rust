d1ef8943b95bd730a4f350432030cd0440c7b79d
KaosBunny <Niamh.K.Ferns@gmail.com>
2022-05-23 14:03:24 +1200

    Catching up learning rust repo.

--- rust-by-example/src/chapter1.rs
+++ rust-by-example/src/chapter1.rs
@@ -1,16 +1,34 @@
-#[allow(dead_code)]
+use std::fmt;
 
-pub fn hello_world() {
+pub fn _hello_world() {
     println!("Hello World!\nI'm a Rustacean!");
 }
 
-pub fn print_formatting_1() {
+pub fn _print_formatting_1() {
     println!("My name is {0}. {1} {0}.", "Bond", "James");
 }
 
-pub fn print_formatting_2() {
+pub fn _print_formatting_2() {
     struct Pair(i32);
     // let pair = Pair(1);
 
-    println!("The struct now prints: {}!", Pair(3).0);
+    println!("The struct now prints: {}!", Pair(3).0); // Only works with one value.
+}
+
+pub fn _print_formatting_3() {
+    let n = 3.1415926535897932;
+    println!("Pi is roughtly {:.3}", n);
+}
+
+pub fn print_formatting_4() {
+    struct Pair(i32, f64);
+    // let pair = Pair(1);
+
+    impl fmt::Display for Pair {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "V1 = {} V2 = {}", self.0, self.1)
+        }
+    }
+
+    println!("The struct now prints: {}!", Pair(3, 3.14));
 }
--- rust-by-example/src/main.rs
+++ rust-by-example/src/main.rs
@@ -2,6 +2,8 @@ mod chapter1;
 
 fn main() {
     // chapter1::hello_world();
-    chapter1::print_formatting_1();
-    chapter1::print_formatting_2();
+    // chapter1::print_formatting_1();
+    // chapter1::print_formatting_2();
+    // chapter1::print_formatting_3();
+    chapter1::print_formatting_4();
 }
